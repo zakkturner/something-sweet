a1919853c3cddfe56fa477f4607d1e4fa5309218
zakkturner <30674356+zakkturner@users.noreply.github.com>
2021-08-19 20:53:12 -0400

    scripts added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
-<template>
-  <Tutorial/>
-</template>
+<template></template>
 
 <script>
-export default {}
+export default {
+  head() {
+    return {
+      script: [
+        { src: '/vendor/jquery/jquery.min.js' },
+        { src: '/vendor/bootstrap/js/bootstrap.min.js' },
+        { src: '/js/custom.js' },
+        { src: '/js/plugins.js' },
+        { src: '/vendor/layerslider/js/greensock.js' },
+        { src: '/vendor/layerslider/js/layerslider.transitions.js' },
+        { src: '/vendor/layerslider/js/layerslider.kreaturamedia.jquery.js' },
+        { src: '/vendor/layerslider/js/layerslider.load.js' },
+      ],
+      link: [{}],
+    }
+  },
+}
 </script>
